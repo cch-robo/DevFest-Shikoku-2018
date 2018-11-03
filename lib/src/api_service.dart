64fc257138d3eff8e79c7e5da8c3735f8024e0b4
cch-robo <robo@cch-lab.com>
2018-11-03 09:32:12 +0900

    コンテスト犬画像URLを修正

--- lib/src/api_service.dart
+++ lib/src/api_service.dart
@@ -14,16 +14,16 @@ class ApiService {
     /// https://www.irasutoya.com/
     String jsonList = ''
         +'['
-        + '{"dog":{"name":"ボーダーコリー","imageUrl":"http://cch-lab.com/reference/images/dog_Border_Collie.png"},"like":false},'
-        + '{"dog":{"name":"秋田犬","imageUrl":"http://cch-lab.com/reference/images/dog_akitainu.png"},"like":false},'
-        + '{"dog":{"name":"バセットハウンド","imageUrl":"http://cch-lab.com/reference/images/dog_basset_hound.png"},"like":false},'
-        + '{"dog":{"name":"ボストンテリア","imageUrl":"http://cch-lab.com/reference/images/dog_boston_terrier.png"},"like":false},'
-        + '{"dog":{"name":"ブルテリア","imageUrl":"http://cch-lab.com/reference/images/dog_bull_terrier.png"},"like":false},'
-        + '{"dog":{"name":"ダルメシアン","imageUrl":"http://cch-lab.com/reference/images/dog_dalmatian.png"},"like":false},'
-        + '{"dog":{"name":"シーズー","imageUrl":"http://cch-lab.com/reference/images/dog_shih_tzu.png"},"like":false},'
-        + '{"dog":{"name":"スピッツ","imageUrl":"http://cch-lab.com/reference/images/dog_spitz.png"},"like":false},'
-        + '{"dog":{"name":"土佐犬","imageUrl":"http://cch-lab.com/reference/images/dog_tosaken.png"},"like":false},'
-        + '{"dog":{"name":"柴犬","imageUrl":"http://cch-lab.com/reference/images/dog_shibainu_brown.png"},"like":false}'
+        + '{"dog":{"name":"ボーダーコリー","imageUrl":"https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_Border_Collie.png"},"like":false},'
+        + '{"dog":{"name":"秋田犬","imageUrl":"https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_akitainu.png"},"like":false},'
+        + '{"dog":{"name":"バセットハウンド","imageUrl":"https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_basset_hound.png"},"like":false},'
+        + '{"dog":{"name":"ボストンテリア","imageUrl":"https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_boston_terrier.png"},"like":false},'
+        + '{"dog":{"name":"ブルテリア","imageUrl":"https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_bull_terrier.png"},"like":false},'
+        + '{"dog":{"name":"ダルメシアン","imageUrl":"https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_dalmatian.png"},"like":false},'
+        + '{"dog":{"name":"シーズー","imageUrl":"https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_shih_tzu.png"},"like":false},'
+        + '{"dog":{"name":"スピッツ","imageUrl":"https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_spitz.png"},"like":false},'
+        + '{"dog":{"name":"土佐犬","imageUrl":"https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_tosaken.png"},"like":false},'
+        + '{"dog":{"name":"柴犬","imageUrl":"https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_shibainu_brown.png"},"like":false}'
         + ']';
 
     List<dynamic> dynamicList = json.decode(jsonList);
@@ -45,16 +45,16 @@ class ApiService {
     /// かわいいフリー素材集 いらすとや
     /// https://www.irasutoya.com/
     return [
-      new ContestDog("ボーダーコリー", "http://cch-lab.com/reference/images/dog_Border_Collie.png", false),
-      new ContestDog("秋田犬", "http://cch-lab.com/reference/images/dog_akitainu.png", false),
-      new ContestDog("バセットハウンド", "http://cch-lab.com/reference/images/dog_basset_hound.png", false),
-      new ContestDog("ボストンテリア", "http://cch-lab.com/reference/images/dog_boston_terrier.png", false),
-      new ContestDog("ブルテリア", "http://cch-lab.com/reference/images/dog_bull_terrier.png", false),
-      new ContestDog("ダルメシアン", "http://cch-lab.com/reference/images/dog_dalmatian.png", false),
-      new ContestDog("シーズー", "http://cch-lab.com/reference/images/dog_shih_tzu.png", false),
-      new ContestDog("スピッツ", "http://cch-lab.com/reference/images/dog_spitz.png", false),
-      new ContestDog("土佐犬", "http://cch-lab.com/reference/images/dog_tosaken.png", false),
-      new ContestDog("柴犬", "http://cch-lab.com/reference/images/dog_shibainu_brown.png", false),
+      new ContestDog("ボーダーコリー", "https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_Border_Collie.png", false),
+      new ContestDog("秋田犬", "https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_akitainu.png", false),
+      new ContestDog("バセットハウンド", "https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_basset_hound.png", false),
+      new ContestDog("ボストンテリア", "https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_boston_terrier.png", false),
+      new ContestDog("ブルテリア", "https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_bull_terrier.png", false),
+      new ContestDog("ダルメシアン", "https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_dalmatian.png", false),
+      new ContestDog("シーズー", "https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_shih_tzu.png", false),
+      new ContestDog("スピッツ", "https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_spitz.png", false),
+      new ContestDog("土佐犬", "https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_tosaken.png", false),
+      new ContestDog("柴犬", "https://cch-robo.github.io/DevFest-Shikoku-2018/images/dog_shibainu_brown.png", false),
     ];
   }
 
