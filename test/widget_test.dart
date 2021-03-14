875fbfb5a06e4203cdcd6345af882ec5b75885f7
cch-robo <robo@cch-lab.com>
2021-03-14 09:52:54 +0900

    (2021/03版) 対応テスト追加

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:devfest_shikoku_2018/main.dart';
+import '../test/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
